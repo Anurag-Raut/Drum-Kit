0868d85d46b04f93410416b1e6a0f1ba47d8c1ed
Anurag-Raut <102049482+Anurag-Raut@users.noreply.github.com>
2022-05-13 21:42:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+var num=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<num;i++){
+                                                                                                                                        
+    document.querySelectorAll(".drum")[i].addEventListener("keydown",function(event){
+        
+        // var letter =this.innerHTML;
+        var letter =event.key;
+    
+        switch (letter) {
+            case "w":
+                var tom2 = new Audio("sounds/tom-1.mp3");
+                tom2.play();
+                break;
+        
+                case "a":
+                var tom1 = new Audio("sounds/tom-2.mp3");
+                tom1.play();
+                break;
+
+                case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+                case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+                case "j":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "k":
+                var snare = new Audio("sounds/kick-bass.mp3");
+                snare.play();
+                break;
+
+
+            case "l":
+                var kick = new Audio("sounds/snare.mp3");
+                kick.play();
+                break;    
+            default:
+                break;
+        }
+
+    });
+
+}
+
+for(var i=0;i<num;i++){
+                                                                                                                                        
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(event){
+        
+        var letter =this.innerHTML;
+        // var letter =event.key;
+    
+        switch (letter) {
+            case "w":
+                var tom2 = new Audio("sounds/tom-1.mp3");
+                tom2.play();
+                break;
+        
+                case "a":
+                var tom1 = new Audio("sounds/tom-2.mp3");
+                tom1.play();
+                break;
+
+                case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+                case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+                case "j":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "k":
+                var snare = new Audio("sounds/kick-bass.mp3");
+                snare.play();
+                break;
+
+
+            case "l":
+                var kick = new Audio("sounds/snare.mp3");
+                kick.play();
+                break;    
+            default:
+                break;
+        }
+
+    });
+
+}
